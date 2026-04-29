fcd283ac210006925ded7a24142910952a7b2ebd
larajmeurers <lara.meurer.souza@escola.pr.gov.br>
2026-04-29 08:42:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -29,3 +29,30 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("2027-05-07T00:00:00");
+let tempoAtual = new Date(2026-04-08T00:00:00);
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+}
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+return (
+  dias +
+  " dias " +
+  horas +
+  " horas " +
+  minutos +
+  " minutos " +
+  segundos +
+  " segundos"
+);
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
